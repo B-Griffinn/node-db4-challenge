f94dd3671eb4a1cac6222a58963a611a9884496d
Ben <bigriffin94@outlook.com>
2019-10-10 14:44:59 -0700

    Migrations Initial Migration added

--- knexfile.js
+++ knexfile.js
@@ -3,11 +3,19 @@
 module.exports = {
 
   development: {
+    // client - database driver
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
-    }
+      filename: './data/recipes.db3'  // << creates a db3 file for me
+    },
+    useNullAsDefault: true,
   },
+
+/*
+  We’ll need to update the location (or desired location) of the database(which
+  is the filename:) as well as add the useNullAsDefault option. The latter option
+  prevents crashes when working with sqlite3.
+*/
 
   staging: {
     client: 'postgresql',
@@ -26,7 +34,7 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
+    client: 'sqlite3',
     connection: {
       database: 'my_db',
       user:     'username',
